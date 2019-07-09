111041d7c651fa4449062b2abe41f8b44187dd75
Jonas Bygdén <github@bygden.nu>
2019-07-09 16:14:13 +0200

    Added after switch

---
+++ dotfiles/config.fish
@@ -0,0 +1,16 @@
+set -gx LC_ALL en_US.UTF-8
+set -gx LANG en_US.UTF-8
+alias brew_check='brew update && brew outdated'
+alias brewu='brew update && brew upgrade && brew cleanup && brew doctor'
+alias df='/usr/local/bin/gdf -h'
+alias du='/usr/local/bin/gdu -h'
+alias emacs='emacs -nw'
+alias ls='ls -FG'
+alias l='ls -lFG'
+alias la='ls -lFGa'
+alias m='less -iM'
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+test -e /usr/local/bin/archey ; and /usr/local/bin/archey -o
