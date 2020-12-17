74ba60d8472f6d3f1eacc6e4fa4f8ca576f05f3b
Jonas Bygdén <github@bygden.nu>
2020-12-17 22:01:13 +0100

    Updated

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,7 +1,7 @@
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
-alias brew_check='brew update && brew outdated && brew cask outdated'
-alias brewu='brew update && brew upgrade && brew cask upgrade && brew cleanup && brew doctor'
+alias brew_check='brew update && brew outdated && brew outdated --cask'
+alias brewu='brew update && brew upgrade && brew upgrade --cask && brew cleanup && brew doctor'
 alias df='/usr/local/bin/gdf -h'
 alias du='/usr/local/bin/gdu -h'
 alias emacs='emacs -nw'
@@ -13,3 +13,4 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
+test -e {$HOME}/src/github/fish-ssh-agent/init.fish; and source {$HOME}/src/github/fish-ssh-agent/init.fish
